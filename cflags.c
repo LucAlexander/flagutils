04da356d57d84343e1724f8e502b514b19663a32
LucAlexander <lucskyalex@gmail.com>
2022-04-20 16:39:04 -0400

    bit count

--- cflags.c
+++ cflags.c
@@ -21,9 +21,42 @@ uint64_t bit_set(uint64_t flag, uint8_t pos, uint8_t bit){
 	return ((flag & ~(1<<(pos-1))) | (bit << (pos-1)));
 }
 
-void printflag(uint64_t flag){
+uint8_t bit_count(uint64_t flag){
+	uint8_t count = 0;
+	while (flag){
+		flag &= (flag-1);
+		count++;
+	}
+	return count;
+}
+
+void printflag64(uint64_t flag){
 	int8_t i;
-	for (i = FLAG_OPERATION_BITWIDTH;i>0;--i){
+	for (i = FOB_64;i>0;--i){
+		printf("%u",bit_check(flag, i));
+	}
+	printf("\n");
+}
+
+void printflag32(uint32_t flag){
+	int8_t i;
+	for (i = FOB_32;i>0;--i){
+		printf("%u",bit_check(flag, i));
+	}
+	printf("\n");
+}
+
+void printflag16(uint16_t flag){
+	int8_t i;
+	for (i = FOB_16;i>0;--i){
+		printf("%u",bit_check(flag, i));
+	}
+	printf("\n");
+}
+
+void printflag8(uint8_t flag){
+	int8_t i;
+	for (i = FOB_8;i>0;--i){
 		printf("%u",bit_check(flag, i));
 	}
 	printf("\n");
--- cflags.h
+++ cflags.h
@@ -3,7 +3,10 @@
 
 #include <stdint.h>
 
-#define FLAG_OPERATION_BITWIDTH 64
+#define FOB_64 64
+#define FOB_32 32
+#define FOB_16 16
+#define FOB_8 8
 
 uint64_t bit_flip(uint64_t flag, uint8_t bit);
 uint64_t bit_off(uint64_t flag, uint8_t bit);
@@ -11,7 +14,12 @@ uint64_t bit_on(uint64_t flag, uint8_t bit);
 uint8_t bit_check(uint64_t flag, uint8_t bit);
 uint64_t bit_set(uint64_t flag, uint8_t pos, uint8_t bit);
 
-void printflag(uint64_t flag);
+uint8_t bit_count(uint64_t flag);
+
+void printflag64(uint64_t flag);
+void printflag32(uint32_t flag);
+void printflag16(uint16_t flag);
+void printflag8(uint8_t flag);
 
 #endif
 
